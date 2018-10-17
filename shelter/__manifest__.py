e99e40c5cbcf3c0f28dfcd4a0125566915ccacb2
Denis Vermylen <dve@odoo.com>
2018-10-18 00:02:32 +0200

    add shelter module

---
+++ shelter/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "shelter",
+
+    'summary': """
+        Shelter app, appointments, patients.""",
+
+    'description': """
+        This app provides the features needed by any shelter.
+    """,
+
+    'author': "Denis Ledoux",
+    'website': "http://www.ldx.be",
+    'category': 'Shelter',
+    'version': '0.1',
+    'depends': ['base', 'mail'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/common.xml',
+        'views/animal.xml',
+        'views/patient.xml',
+    ],
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
